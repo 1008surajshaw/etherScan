46a151015cc08a711c21b50e878e935513523905
suraj <iamsuraj3000@gmail.com>
2024-10-23 18:57:31 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
 
+//INTERNAL IMPORT
+import NavBar from "../components/NavBar"
+import Footer from "../components/Footer"
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return <div>
+           <NavBar/>
+           <Component {...pageProps} />
+           <Footer/>
+        </div> 
 }
